84c73a48b4f6241c615436da1c3aa33999ce6b26
jdm79 <james.malvern@gmail.com>
2018-09-06 15:53:01 +0100

    adds index.js to index.html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 $( "#up" ).click(function() {
   console.log("up button clicked by the user on this laptop..; ")
 })
+
+$( "#down" ).click(function() {
+  console.log("down button clicked by the user on this laptop..; ")
+})
